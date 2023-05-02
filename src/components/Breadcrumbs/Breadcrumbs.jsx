65c4995ca92e1ae511fa9378e3e9027167276988
Angel Andrade <aandradeb99@gmail.com>
2023-05-02 17:16:38 +0200

    Create a request to retrieve all countries and display them

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 import styles from './Breadcrumbs.module.css';
 
 const Breadcrumbs = (props) => {
--- src/screens/Countries/Countries.jsx
+++ src/screens/Countries/Countries.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import Header from "../../components/Header/Header";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import Crumb from "../../components/Crumb/Crumb";
@@ -6,14 +9,88 @@ import Footer from "../../components/Footer/Footer";
 import styles from './Countries.module.css';
 
 const Countries = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [query, setQuery] = useState("");
+  const [searchParam] = useState(["capital", "name", "numericCode"]);
+  const [filterParam, setFilterParam] = useState(["All"]);
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(function (response) {
+        setIsLoaded(true);
+        setItems(response.data);
+      })
+      .catch(function (error) {
+        setIsLoaded(true);
+        setError(error);
+      });
+  }, []);
+
+  function search(items) {
+    return items.filter((item) => {
+      if (item.region === filterParam) {
+        return searchParam.some((newItem) => {
+          return (
+            item[newItem]
+              .toString()
+              .toLowerCase()
+              .indexOf(query.toLowerCase()) > -1
+          );
+        });
+      } else if (filterParam === "All") {
+        return searchParam.some((newItem) => {
+          return (
+            item[newItem]
+              .toString()
+              .toLowerCase()
+              .indexOf(query.toLowerCase()) > -1
+          );
+        });
+      }
+    });
+  }
+
+  if (!isLoaded) {
+    return <>loading...</>;
+  }
 
   return (
     <>
       <Header />
       <main className={styles.main}>
         <Breadcrumbs lastCrumb="Countries">
-          <Crumb to="/" name="Home"/>
+          <Crumb to="/" name="Home" />
         </Breadcrumbs>
+        <h2 className={styles.main__title}>All countries</h2>
+        <div className={styles.main__wrapper}>
+          <input
+            type="search"
+            name="search-form"
+            id="search-form"
+            className={styles.main__searchInput}
+            placeholder="Search by country or capital"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <div className={styles.main__countries}>
+            {items.map((item, number) => (
+              <div className={styles.card} key={number}>
+                <div className={styles.cardImage}>
+                  <img
+                    src={item.flags.svg}
+                    alt={item.flags.alt}
+                  />
+                </div>
+                <div className={styles.cardContent}>
+                  <h2 className={styles.cardName}>{item.name.common}</h2>
+                  <p className={styles.cardCapital}>{item.capital}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
       <Footer />
     </>
